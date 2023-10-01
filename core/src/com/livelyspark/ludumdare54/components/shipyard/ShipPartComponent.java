79aee02befafbe5826b3cbaf1a27768c0c1d4cb1
Mike <darlington_mike@hotmail.com>
2023-10-01 20:57:22 +0100

    Part removal added

--- core/src/com/livelyspark/ludumdare54/components/shipyard/ShipPartComponent.java
+++ core/src/com/livelyspark/ludumdare54/components/shipyard/ShipPartComponent.java
@@ -2,9 +2,11 @@ package com.livelyspark.ludumdare54.components.shipyard;
 
 import com.badlogic.ashley.core.Component;
 import com.livelyspark.ludumdare54.shipconstruction.ShipPartBase;
+import com.livelyspark.ludumdare54.shipconstruction.ShipPartFitted;
 
 public class ShipPartComponent implements Component {
     public ShipPartBase Part;
+    public ShipPartFitted PartFitted;
     public int OriginX;
     public int OriginY;
 }
