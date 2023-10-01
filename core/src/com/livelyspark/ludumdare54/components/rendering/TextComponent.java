3eb9e5a174f1d7be5fecb18930400309ab2e6f8c
Andrew Darlington <Andrew.Darlington@denca.com>
2023-10-01 10:49:57 +0100

    Text component

--- core/src/com/livelyspark/ludumdare54/components/rendering/TextComponent.java
+++ core/src/com/livelyspark/ludumdare54/components/rendering/TextComponent.java
@@ -4,16 +4,17 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import com.livelyspark.ludumdare54.enums.Shapes;
 
-public class ShapeComponent implements Component {
+public class TextComponent implements Component {
 
-    public Shapes shape;
+    public String text;
     public Color color;
     public float size;
     public Vector2 positionOffset;
 
-    public ShapeComponent(Shapes shape, Color color, float size, Vector2 positionOffset)
+
+    public TextComponent(String text, Color color, float size, Vector2 positionOffset)
     {
-        this.shape = shape;
+        this.text = text;
         this.color = color;
         this.size = size;
         this.positionOffset = positionOffset;
