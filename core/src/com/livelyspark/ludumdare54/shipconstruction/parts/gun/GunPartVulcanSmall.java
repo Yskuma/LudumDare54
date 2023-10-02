2bd498b09896ed2a42665420876696c1a1a1e6c1
Mike <darlington_mike@hotmail.com>
2023-10-02 22:50:43 +0100

    Balance tweaks

--- core/src/com/livelyspark/ludumdare54/shipconstruction/parts/gun/GunPartVulcanSmall.java
+++ core/src/com/livelyspark/ludumdare54/shipconstruction/parts/gun/GunPartVulcanSmall.java
@@ -28,7 +28,7 @@ public class GunPartVulcanSmall extends GunPartBase {
         cost = StaticConstants.costLookup.get(CostTiers.Pricey);
 
         cooldownMax = 0.1f;
-        energyUsage = 1.0f;
+        energyUsage = 2.0f;
         soundKey = SoundKeys.Pew;
 
         usedSlots[0][0] = true;
