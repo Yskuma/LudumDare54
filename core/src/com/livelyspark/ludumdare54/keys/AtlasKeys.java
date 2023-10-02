a830a1722d8b46361a5b357c1f781e79e6b59c3e
Andrew Darlington <Andrew.Darlington@denca.com>
2023-10-02 09:48:38 +0100

    Asteroids

--- core/src/com/livelyspark/ludumdare54/keys/AtlasKeys.java
+++ core/src/com/livelyspark/ludumdare54/keys/AtlasKeys.java
@@ -11,6 +11,18 @@ public class AtlasKeys {
 
     public static String Ship001 = "ship-001";
     public static String Ship002 = "ship-002";
+    public static String Ship003 = "ship-003";
+    public static String Ship004 = "ship-004";
 
     public static String Mine = "mine";
+
+    public static String Asteroid_Brown_8_8 = "asteroid-brown-8-8";
+    public static String Asteroid_Brown_8_16 = "asteroid-brown-8-16";
+    public static String Asteroid_Brown_16_8 = "asteroid-brown-16-8";
+    public static String Asteroid_Brown_16_16 = "asteroid-brown-16-16";
+    public static String Asteroid_Brown_20_20 = "asteroid-brown-20-20";
+    public static String Asteroid_Brown_32_32 = "asteroid-brown-32-32";
+
+
+    public static String Asteroid_Grey = "asteroid-grey-001";
 }
