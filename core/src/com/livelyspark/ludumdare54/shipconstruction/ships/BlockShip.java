24b9674d3ae992fbb1c11c4d3c5d1891ee6e3ef6
Mike <darlington_mike@hotmail.com>
2023-10-02 21:45:46 +0100

    Changed ship slots

--- core/src/com/livelyspark/ludumdare54/shipconstruction/ships/BlockShip.java
+++ core/src/com/livelyspark/ludumdare54/shipconstruction/ships/BlockShip.java
@@ -18,6 +18,38 @@ public class BlockShip extends ShipBase {
             }
         }
 
-        partSlots[6][6]= false;
+        partSlots[7][7]= false;
+        partSlots[7][8]= false;
+        partSlots[7][9]= false;
+        partSlots[8][7]= false;
+        partSlots[8][8]= false;
+        partSlots[8][9]= false;
+
+        partSlots[4][11]= false;
+        partSlots[5][11]= false;
+        partSlots[10][11]= false;
+        partSlots[11][11]= false;
+
+        partSlots[4][9]= false;
+        partSlots[4][10]= false;
+        partSlots[11][9]= false;
+        partSlots[11][10]= false;
+
+        partSlots[6][4]= false;
+        partSlots[7][4]= false;
+        partSlots[8][4]= false;
+        partSlots[9][4]= false;
+
+        partSlots[7][12]= true;
+        partSlots[7][13]= true;
+        partSlots[8][12]= true;
+        partSlots[8][13]= true;
+
+        partSlots[3][4]= true;
+        partSlots[3][5]= true;
+        partSlots[3][6]= true;
+        partSlots[12][4]= true;
+        partSlots[12][5]= true;
+        partSlots[12][6]= true;
     }
 }
